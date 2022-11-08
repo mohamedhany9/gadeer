f9b7aecf8894717d1d3506b6178853427eb8a38d
Mohamed Hany <maohmedh@gmail.com>
2022-11-08 02:13:42 +0200

    Consultants Page

--- lib/modules/consultants_home/contsultants_page.dart
+++ lib/modules/consultants_home/contsultants_page.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_bloc/flutter_form_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gadeer/component/app_bar.dart';
+import 'package:gadeer/component/input_decoration.dart';
+import 'package:gadeer/data/model/category.model.dart';
 import 'package:gadeer/data/model/profile.model.dart';
+import 'package:gadeer/data/request/consulting/search_consult.request.dart';
 import 'package:gadeer/data/response/consulting/add_consulting.response.dart';
 import 'package:gadeer/helper/constants.dart';
 import 'package:gadeer/helper/notifications.dart';
 import 'package:gadeer/modules/consultants_home/widget/all_constultants.dart';
+import 'package:gadeer/modules/consulting/service/consulting_service.dart';
 import 'package:gadeer/modules/consulting/widgets/add_consulting_form/category_selection.widget.dart';
+import 'package:gadeer/modules/select_consultant/widgets/search_selectable_cosnultants_field.dart';
+import 'package:gadeer/modules/select_consultant/widgets/selectable_consultant_item.widget.dart';
+import 'package:get/get.dart';
 
 import '../consulting/forms/consulting_create_form.bloc.dart';
 
@@ -23,7 +31,13 @@ class _ConsultantsPageState extends State<ConsultantsPage> {
 
   ConsultingCreateFormBloc? formBloc;
 
+
+  final consultingService = Get.find<ConsultingService>();
+  final filters = ["الاعلي تقييما", "الاقل تقييما"];
+  List<ProfileModel>? consultants = [];
+  int? selectedId;
   List<int?> selctedCats = [];
+  TextEditingController search = TextEditingController();
 
   @override
   void initState() {
@@ -34,15 +48,15 @@ class _ConsultantsPageState extends State<ConsultantsPage> {
 
     if (formBloc!.category.value != null) {
       selctedCats.add(formBloc!.category.value!.id);
-    }
-    if (formBloc!.subCategory.value != null) {
-      selctedCats.add(formBloc!.subCategory.value!.id);
+      print(selctedCats);
     }
 
-    print("hany");
-    print(formBloc!.consultant);
+    Future.microtask(() async {
+      await _searchConsultants(null);
+    });
     super.initState();
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -64,7 +78,7 @@ class _ConsultantsPageState extends State<ConsultantsPage> {
         child: FormBlocListener<ConsultingCreateFormBloc, AddConsultingResponse,
             Object>(
           formBloc: formBloc,
-          onSubmitting: (_, __) => Notifications.showLoading(),
+         // onSubmitting: (_, __) => Notifications.showLoading(),
           onSuccess: formBloc!.onSuccess,
           onFailure: formBloc!.onFailure,
           child: Padding(
@@ -73,10 +87,119 @@ class _ConsultantsPageState extends State<ConsultantsPage> {
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
               child: Column(
                 children: [
-                  CategorySelectionWidget(formBloc),
-                  Expanded(
+                  Column(
+                    children: [
+                      DropdownFieldBlocBuilder<CategoryModel>(
+                        selectFieldBloc: formBloc!.category,
+                        showEmptyItem: false,
+                        decoration: inputDecoration(
+                          label: 'مجال الاستشارة',
+                          icon: FontAwesomeIcons.clipboard,
+                          borderRadius: 5,
+                        ),
+                        onChanged: (c){
+                          setState(() {
+                            selctedCats = [];
+                            selctedCats.add(c!.id);
+                          });
+                          Future.microtask(() async {
+                            await _searchConsultants(null);
+                          });
+                        },
+                        itemBuilder: (context, cat) => cat.title!,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      DropdownFieldBlocBuilder<CategoryModel>(
+                        selectFieldBloc: formBloc!.subCategory,
+                        decoration: inputDecoration(
+                          label: 'المجال الثانوي',
+                          hint: '-- الكل --',
+                          icon: FontAwesomeIcons.clipboard,
+                          borderRadius: 5,
+                        ),
+                        itemBuilder: (context, sub) => sub.title ?? "",
+                      ),
+                    ],
+                  ),
+                   Expanded(
                       child
-                      : AllConsultantPage([]))
+                      : Container(
+                        height: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          image: DecorationImage(
+                            repeat: ImageRepeat.repeat,
+                            image: AssetImage(
+                              Constants.background1,
+                            ),
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 16,
+                            ),
+                            SearchSelectableConsultantsField(
+                              search: search,
+                              filters: filters,
+                              onEditingComplete: (s) async {
+                                print(s);
+                                FocusScope.of(context).unfocus();
+                                await _searchConsultants(search.text);
+                              },
+                              onSelected: (filter) {
+                                if (filter == "الاعلي تقييما") {
+                                  consultants?.sort((pro1, pro2) {
+                                    return (pro2.rate ?? 0.0).compareTo((pro1.rate ?? 0.0));
+                                  });
+                                } else {
+                                  consultants?.sort((pro1, pro2) {
+                                    return (pro1.rate ?? 0.0).compareTo((pro2.rate ?? 0.0));
+                                  });
+                                }
+                                setState(() {});
+                              },
+                            ),
+                            SizedBox(
+                              height: 16,
+                            ),
+                            Expanded(
+                                child: ListView.builder(
+                                    itemCount: consultants!.length,
+                                    itemBuilder: (c, i) {
+                                      return SelectableConsultantItemWidget(
+                                        consultants![i],
+                                            (id) {
+                                          this.selectedId = id;
+                                          setState(() {});
+                                        },
+                                        selected: consultants?[i].id == selectedId,
+                                      );
+                                    })),
+                            // Padding(
+                            //   padding: const EdgeInsets.all(8.0),
+                            //   child: CustomButton(
+                            //     selectedId == null
+                            //         ? "اختيار الخبير"
+                            //         : " تعيين الخبير ${selectedId == null ? "" : consultants?.firstWhere((element) => element.id == selectedId).name}",
+                            //         () {
+                            //       ProfileModel? selected;
+                            //       try {
+                            //         selected = consultants
+                            //             ?.firstWhere((element) => element.id == selectedId);
+                            //       } catch (e) {
+                            //         print(e.toString());
+                            //       }
+                            //       Get.back(result: selected);
+                            //     },
+                            //     color: selectedId == null ? Colors.grey : AppColors.primary,
+                            //   ),
+                            // ),
+                          ],
+                        ),
+                      ))
                 ],
               ),
             ),
@@ -84,5 +207,24 @@ class _ConsultantsPageState extends State<ConsultantsPage> {
         ),
       ),
     );
+  }
+
+  Future _searchConsultants(String? jobTitle) async {
+    Notifications.showLoading();
+    await consultingService
+        .searchConsulting(
+        ConsultingSearchRequest(categories: selctedCats, jobTitle: jobTitle ?? ""))
+        .then((value) {
+      Notifications.hideLoading();
+
+      consultants = value.data;
+
+      setState(() {});
+    }).catchError((e) {
+      print(e.toString());
+      Notifications.hideLoading();
+
+      Notifications.error(Constants.netError);
+    });
   }
 }
